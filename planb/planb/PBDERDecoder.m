ba8e54e5e103a67af2a030507994b4486296ee42
Jack Flintermann <jack@stripe.com>
2015-08-28 15:44:18 -0400

    remove trailing whitespace

--- planb/planb/PBDERDecoder.m
+++ planb/planb/PBDERDecoder.m
@@ -79,7 +79,7 @@
 /*
  The DER data provided by NSURLProtectionSpace.distinguishedNames looks like
  this:
- 
+
  SEQUENCE {
   SET {
     SEQUENCE {
@@ -94,7 +94,7 @@
     }
   }
  }
- 
+
  This method assumes the passed in data will be in that format. If it isn't,
  the DER decoding will fail and this method will return nil.
  */
@@ -103,38 +103,38 @@
     SecAsn1Oid oid;
     SecAsn1Item value;
   } OIDKeyValue;
-  
+
   static const SecAsn1Template kOIDValueTemplate[] = {
     { SEC_ASN1_SEQUENCE, 0, NULL, sizeof(OIDKeyValue) },
     { SEC_ASN1_OBJECT_ID, offsetof(OIDKeyValue, oid), NULL, 0 },
     { SEC_ASN1_ANY_CONTENTS, offsetof(OIDKeyValue, value), NULL, 0 },
     { 0, 0, NULL, 0 }
   };
-  
+
   typedef struct {
     OIDKeyValue **vals;
   } OIDKeyValueList;
-  
+
   static const SecAsn1Template kSetOfOIDValueTemplate[] = {
     { SEC_ASN1_SET_OF, 0, kOIDValueTemplate, sizeof(OIDKeyValueList) },
     { 0, 0, NULL, 0 }
   };
-  
+
   typedef struct {
     OIDKeyValueList **lists;
   } OIDKeyValueListSeq;
-  
+
   static const SecAsn1Template kSequenceOfSetOfOIDValueTemplate[] = {
     { SEC_ASN1_SEQUENCE_OF, 0, kSetOfOIDValueTemplate, sizeof(OIDKeyValueListSeq) },
     { 0, 0, NULL, 0 }
   };
-  
+
   OSStatus err = errSecSuccess;
   SecAsn1CoderRef coder;
-  
+
   err = SecAsn1CoderCreate(&coder);
   if (err != errSecSuccess) return nil;
-  
+
   OIDKeyValueListSeq a;
   err = SecAsn1Decode(coder,
                       data.bytes,
@@ -145,20 +145,20 @@
     SecAsn1CoderRelease(coder);
     return nil;
   }
-  
+
   // The data is decoded but now it's in a number of embedded structs.
   // Massage that into a nice dictionary of OID->String pairs.
   NSMutableDictionary *dict = [NSMutableDictionary dictionary];
   OIDKeyValueList *anAttr;
   for (NSUInteger i = 0; (anAttr = a.lists[i]); i++) {
     OIDKeyValue *keyValue = anAttr->vals[0];
-    
+
     // Sanity check
     if (keyValue->value.Length > data.length) {
       SecAsn1CoderRelease(coder);
       return nil;
     }
-    
+
     // Get the string value. First try creating as a UTF-8 string. If that fails,
     // fallback to trying as an ASCII string. If it still doesn't work, continue on
     // to the next value.
@@ -172,15 +172,15 @@
                                            encoding:NSASCIIStringEncoding];
     }
     if (!valueString) continue;
-    
+
     // The OID is still encoded, so we need to decode it.
     NSString *objectId = [PBDERDecoder decodeOIDWithBytes:keyValue->oid.Data
                                                    length:keyValue->oid.Length];
-    
+
     // Add to the dictionary
     dict[objectId] = valueString;
   }
-  
+
   SecAsn1CoderRelease(coder);
   return dict;
 }
