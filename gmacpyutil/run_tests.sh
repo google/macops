f48aebf4002de5b27f9089109e1208f003a7f737
Will Jenkins <wijenkins@expedia.com>
2017-03-27 18:18:35 +0100

    Add the PyObjC frameworks to test runner

--- gmacpyutil/run_tests.sh
+++ gmacpyutil/run_tests.sh
@@ -6,4 +6,5 @@ sudo pip install -q pyyaml
 sudo pip install mock
 sudo pip install mox
 sudo pip install google_apputils
+sudo pip install pyobjc-core pyobjc-framework-CoreWLAN pyobjc-framework-Cocoa
 python -m unittest discover -s gmacpyutil -p '*_test.py' -t .
