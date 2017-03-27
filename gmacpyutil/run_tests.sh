b5fbce81b52afbaaf667c74219800c759405311f
Will Jenkins <wijenkins@expedia.com>
2017-03-27 18:31:20 +0100

    Adding pyopenssl and pyobjc-framework-SystemConfiguration

--- gmacpyutil/run_tests.sh
+++ gmacpyutil/run_tests.sh
@@ -6,5 +6,6 @@ sudo pip install -q pyyaml
 sudo pip install mock
 sudo pip install mox
 sudo pip install google_apputils
-sudo pip install pyobjc-core pyobjc-framework-CoreWLAN pyobjc-framework-Cocoa
+sudo pip install pyopenssl
+sudo pip install pyobjc-core pyobjc-framework-CoreWLAN pyobjc-framework-Cocoa pyobjc-framework-SystemConfiguration
 python -m unittest discover -s gmacpyutil -p '*_test.py' -t .
